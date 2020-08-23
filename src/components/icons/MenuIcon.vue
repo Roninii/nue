999cd1badac0860addc885b68d239240e815cf30
Ronnie Villarini <>
2020-08-23 12:34:48 -0500

    add menu icon component, reinit with vue cli & testing

---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="menu-alt1 w-6 h-6"
+        data-darkreader-inline-fill=""
+        style="--darkreader-inline-fill:currentColor;"
+    >
+        <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd"
+        ></path>
+    </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'MenuIcon',
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    </div>
+    <main class="min-h-screen grid place-center text-snow-1">
+        <header class="w-full flex flex-col items-center">
+            <h1 class="text-5xl font-semibold tracking-wide">NÜE</h1>
+            <img alt="Nue logo" src="@/assets/Nue.svg" class="logo w-2/3 sm:w-1/3 lg:w-1/4 py-8" />
+            <h2 class="font-light text-center">
+                A <span class="font-normal text-frost-1">Nordic</span> UI Theme for Vue and
+                TailwindCSS.
+            </h2>
+        </header>
+    </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-    name: 'Home',
-    components: {
-        HelloWorld,
-    },
+    name: 'App',
 });
 </script>
+
+<style lang="postcss">
+body {
+    background: #242933;
+    @apply text-snow-0;
+}
+.logo {
+    filter: drop-shadow(0px 3px 6px rgba(15, 17, 21, 0.2));
+}
+</style>
