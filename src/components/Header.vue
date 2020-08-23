3f85cb64730c1b67bfb41b0ef2369348f92e4528
Ronnie Villarini <>
2020-08-23 18:41:04 -0500

    add form inputs route

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="h-16">
+        <img src="@/assets/Nue.svg" alt="Nue logo" class="hidden lg:block h-full p-4" />
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
+
+<style scoped></style>
